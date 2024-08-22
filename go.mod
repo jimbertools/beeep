e08c9a603c1bcee9446d7b8a3065b7ca703042e7
VantorreWannes <vantorrewannes@gmail.com>
2024-08-22 14:20:16 +0200

    feature: Implement linux notification logic
    - Implement linux notification logic

--- go.mod
+++ go.mod
@@ -2,11 +2,9 @@ module github.com/jimbertools/beeep
 
 go 1.23.0
 
-require github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4
-
-require gopkg.in/urfave/cli.v1 v1.20.0 // indirect
-
 require (
+	github.com/godbus/dbus/v5 v5.1.0
 	github.com/jimbertools/toast v0.0.0
-	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 )
+
+require github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
---
+++ pkg/beeep/notification/linux.go
@@ -0,0 +1,70 @@
+//go:build linux || freebsd || netbsd || openbsd || illumos
+// +build linux freebsd netbsd openbsd illumos
+
+package notification
+
+import (
+	"errors"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/godbus/dbus/v5"
+)
+
+type Notification struct {
+	title string
+	message string
+	iconPath string
+}
+
+func NewNotification(title, message, iconPath string) (*Notification, error) {
+	return &Notification{title: title, message: message, iconPath: iconPath}, nil
+}
+
+func (notification *Notification) Show() error {
+	appIcon, err := filepath.Abs(notification.iconPath)
+	if err != nil {
+		return err
+	}
+
+	cmd := func() error {
+		send, err := exec.LookPath("sw-notify-send")
+		if err != nil {
+			send, err = exec.LookPath("notify-send")
+			if err != nil {
+				return err
+			}
+		}
+
+		c := exec.Command(send, notification.title,	notification.message, "-i", appIcon)
+		return c.Run()
+	}
+
+	knotify := func() error {
+		send, err := exec.LookPath("kdialog")
+		if err != nil {
+			return err
+		}
+		c := exec.Command(send, "--title", notification.title, "--passivepopup", notification.message, "10", "--icon", appIcon)
+		return c.Run()
+	}
+
+	conn, err := dbus.SessionBus()
+	if err != nil {
+		return cmd()
+	}
+	obj := conn.Object("org.freedesktop.Notifications", dbus.ObjectPath("/org/freedesktop/Notifications"))
+
+	call := obj.Call("org.freedesktop.Notifications.Notify", 0, "", uint32(0), appIcon, notification.title, notification.message, []string{}, map[string]dbus.Variant{}, int32(-1))
+	if call.Err != nil {
+		e := cmd()
+		if e != nil {
+			e := knotify()
+			if e != nil {
+				return errors.New("beeep: " + call.Err.Error() + "; " + e.Error())
+			}
+		}
+	}
+
+	return nil
+}
